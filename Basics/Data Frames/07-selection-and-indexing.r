4b165c3e3877082261468449dda35e1352efa09b
DianaAvanesy <52766468+DianaAvanesy@users.noreply.github.com>
2020-01-20 22:48:25 -0500

    Finish ch 7

--- Basics/Data Frames/07-selection-and-indexing.r
+++ Basics/Data Frames/07-selection-and-indexing.r
@@ -1,7 +1,7 @@
 #How to grab data out of data frame
 
-df[1,] #first row 
-df[,1] #first column
+#df[1,] #first row 
+#df[,1] #first column
 
 #One of the main points to use data frames is beeing able to to label your columns and your rows by some sort of character or string names such as days temp or rain
 
@@ -22,4 +22,15 @@ subset(df, subset = rain == TRUE)
 #How to grab days with T>23
 subset(df, subset = temp>23)
 
-#Ordering a data frame
+#How to sort data
+sorted.temp <- order(df['temp'])
+#Output: [1] 1 2 3 5 4
+#You can use this sort of vector to actually sort your data frame
+df[sorted.temp,]
+#Output
+#   days temp  rain
+#1  mon   22  TRUE
+#2  tue   34  TRUE
+#3  wed   43 FALSE
+#5  fri   55 FALSE
+#4 thur   56  TRUE
